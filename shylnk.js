1980b66db3b3a09525160cef8e6ce9b4caa1d663
DarkKnight <94906343+l0v3m0n3y@users.noreply.github.com>
2024-08-22 21:03:31 +0300

    Add files via upload

---
+++ shylnk.js
@@ -0,0 +1,11 @@
+class shylnk{
+    constructor(){
+        this.api = "https://api.shylnk.com/api"
+        this.headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0","content-type":"application/json; charset=UTF-8"}
+    }
+    async short_url(url){
+        let req=await fetch(`${this.api}/url_shortener`,{method:"POST",body:JSON.stringify({"url":url}),headers: this.headers});
+        return await req.json();
+    }
+}
+module.exports = {shylnk};
